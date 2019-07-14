3739ba7e8157a5acf43a634e62ac65aac833c1da
Sakal04 <51100757+Sakal04@users.noreply.github.com>
2019-07-14 13:49:54 +0100

    Update and rename Nato_encode.py to Nato_alphabet.py

--- Nato_alphabet.py
+++ Nato_alphabet.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import string
 c = ("If you can! read!")
 p_dict = {'!': '!', 'A':'Alpha', 'B':'Bravo','C':'Charlie', 'D':'Delta', 'E':'Echo', 'F':'Foxtrot', 'G':'Golf', 'H' : 'Hotel', 'I':'India', 'J':'Juliet', 'K':'Kilo', 'L':'Lima', 'M':'Mike', 'N':'November', 'O':'Oscar', 'P':'Papa', 'Q':'Quebec', 'R':'Romeo', 'S':'Sierra', 'T':'Tango', 'U':'Uniform', 'V':'Victor', 'W':'Whiskey', 'X':'Xray', 'Y':'Yankee', 'Z':'Zulu'}
