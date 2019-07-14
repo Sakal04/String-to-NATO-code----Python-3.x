6561021428b34cd7445a195ed4843ee8ba0c7123
Sakal04 <51100757+Sakal04@users.noreply.github.com>
2019-07-14 02:32:55 +0100

    Create Nato_encode.py

---
+++ Nato_encode.py
@@ -0,0 +1,5 @@
+import string
+c = ("If you can! read!")
+p_dict = {'!': '!', 'A':'Alpha', 'B':'Bravo','C':'Charlie', 'D':'Delta', 'E':'Echo', 'F':'Foxtrot', 'G':'Golf', 'I':'India', 'J':'Juliet', 'K':'Kilo', 'L':'Lima', 'M':'Mike', 'N':'November', 'O':'Oscar', 'P':'Papa', 'Q':'Quebec', 'R':'Romeo', 'S':'Sierra', 'T':'Tango', 'U':'Uniform', 'V':'Victor', 'W':'Whiskey', 'X':'Xray', 'Y':'Yankee', 'Z':'Zulu'}
+st = string.ascii_uppercase + "!"
+print(" ".join([p_dict[char] for char in c.upper().replace(" ", "") if char in st]))
